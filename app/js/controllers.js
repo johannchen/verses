7bf0157e1cae381b04f51dfcbb7cb3404286630a
Johann Chen <johannchen@gmail.com>
2012-07-16 22:16:35 -0700

    add verse

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,17 @@
 
 /* Controllers */
 
+function VerseController($scope) {
+  $scope.verses = [
+    { title: 'John 1:4', content: 'In him was life, and that life was the light of man' },
+    { title: 'Romans 1:16', content: 'I am not ashamed of the gospel, because it is the power of God for the salvation of everyone who believes: first for the Jew, then for the Gentile.'}]
+  
+  $scope.addVerse = function() {
+    $scope.verses.push({title: $scope.verseTitle, content: $scope.verseContent});
+    $scope.verseTitle = '';
+    $scope.verseContent = '';
+  }
+}
 
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
