19e8a823a5652a1a46c11095a345c85d36f4fc57
Johann Chen <johannchen@gmail.com>
2012-07-18 14:46:49 -0700

    localStorage service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,15 +2,14 @@
 
 /* Controllers */
 
-function VersesCtrl($scope) {
-  $scope.verses = [
-    { title: 'John 1:4', content: 'In him was life, and that life was the light of man' },
-    { title: 'Romans 1:16', content: 'I am not ashamed of the gospel, because it is the power of God for the salvation of everyone who believes: first for the Jew, then for the Gentile.'}]
+function VersesCtrl($scope, storage) {
+  $scope.verses = storage.getObject('verses');
   
   $scope.addVerse = function() {
     $scope.verses.push({title: $scope.verseTitle, content: $scope.verseContent});
     $scope.verseTitle = '';
     $scope.verseContent = '';
+		storage.saveObject($scope.verses, 'verses');
   }
 }
 
--- app/js/services.js
+++ app/js/services.js
@@ -8,6 +8,25 @@
 var servicesModule = angular.module('myApp.services', []).
   value('version', '0.1');
 
-servicesModule.factory('IndexedDB', function() {
+servicesModule.factory('storage', function() {
+	var myStorage = {};
 
+	myStorage.getObject = function(key) {
+		var data = [];
+		var json_object = localStorage[key];
+		if (json_object) {
+			data = JSON.parse(json_object);
+		}
+		return data;
+	};
+
+	myStorage.saveObject = function(objectToSave, key) {
+		localStorage[key] = JSON.stringify(objectToSave);
+	};
+
+	myStorage.supported = function() {
+		return 'localStorage' in window && window['localStorage'] !== null;
+	};
+
+	return myStorage;
 });
