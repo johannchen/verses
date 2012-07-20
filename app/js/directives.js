573a874a8179f534b80d0672d38cefbf5864ea6b
Johann Chen <johannchen@gmail.com>
2012-07-19 22:43:38 -0700

    attempt inline edit

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,46 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+var directivesModule = angular.module('myApp.directives', []);
+
+directivesModule.directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+
+directivesModule.directive('jcEditableTextarea', function() {
+  /*
+  var editTemplate = '<textarea ng-show="isEditMode" ng-dblclick="switchToPreview()" rows="10" cols="30" ng-model="editTextArea"></textarea>';
+  var previewTemplate = '<div ng-hide="isEditMode" ng-dblclick="switchToEdit()">Preview</div>';
+  */
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    require: '?ngModel',
+    scope: { 'ngModel': 'attribute'},
+    template: '<textarea rows="10" cols="30" ng-transclude></textarea>'
+    /*
+    compile: function(tElement, tAttrs, transclude) {
+      var editTextArea = tElement.text();
+      tElement.html(editTemplate);
+      var previewElement = angular.element(previewTemplate);
+      tElement.append(previewElement);
+
+      return function(scope, element, attrs) {
+        scope.isEditMode = false;
+        scope.editTextArea = editTextArea;
+
+        scope.switchToPreview = function() {
+          previewElement.html(scope.editTextArea);
+          scope.isEditMode = false;
+        }
+        scope.switchToEdit = function() {
+          scope.isEditMode = true;
+        }
+      }
+    }
+    */
+  }
+});
