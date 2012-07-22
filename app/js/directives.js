0041dc4cfb994125d3cb1c555240594e47c7aa96
Johann Chen <johannchen@gmail.com>
2012-07-22 14:48:13 -0700

    inline edit directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,24 +6,45 @@
 var directivesModule = angular.module('myApp.directives', []);
 
 directivesModule.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+}]);
 
+directivesModule.directive('ngEnter', function() {
+  return function(scope, elm, attrs) {
+    elm.bind('keypress', function(e) {
+      if(e.charCode === 13) scope.$apply(attrs.ngEnter);
+    });
+  };
+});
+
+directivesModule.directive('editableText', function() {
+  return {
+    restrict: 'A',
+    scope: { localModel: '=model' },
+    template: '<span ng-hide="editMode" ng-click="editMode=true">{{localModel}}</span>' +
+      '<input type="text" ng-model="localModel" ng-show="editMode" ng-enter="editMode=false" />'
+  }
+});
+
+directivesModule.directive('editableTextarea', function() {
+  return {
+    restrict: 'A',
+    scope: { localModel: '=model' },
+    template: '<div ng-hide="editMode" ng-click="editMode=true">{{localModel}}</div>' +
+      '<textarea rows="10" cols="50" ng-model="localModel" ng-show="editMode" ng-enter="editMode=false"></textarea>'
+  } 
+});
+
+/* tutorial code 
 directivesModule.directive('jcEditableTextarea', function() {
-  /*
   var editTemplate = '<textarea ng-show="isEditMode" ng-dblclick="switchToPreview()" rows="10" cols="30" ng-model="editTextArea"></textarea>';
   var previewTemplate = '<div ng-hide="isEditMode" ng-dblclick="switchToEdit()">Preview</div>';
-  */
   return {
     restrict: 'E',
-    replace: true,
     transclude: true,
-    require: '?ngModel',
-    scope: { 'ngModel': 'attribute'},
-    template: '<textarea rows="10" cols="30" ng-transclude></textarea>'
-    /*
+    scope: {},
     compile: function(tElement, tAttrs, transclude) {
       var editTextArea = tElement.text();
       tElement.html(editTemplate);
@@ -43,6 +64,6 @@ directivesModule.directive('jcEditableTextarea', function() {
         }
       }
     }
-    */
   }
 });
+*/
