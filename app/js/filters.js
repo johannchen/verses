66f7c5d3f53edc107562d9e7d350a6f73ea0c7d1
Johann Chen <johannchen@gmail.com>
2012-07-27 16:21:41 -0700

    filter for verse bg color

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,15 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }]).
+	filter('bgColor', function() {
+		return function(input, memorizedNum) {
+			var out = "";
+			if (memorizedNum > 0 && memorizedNum < 5) {
+				out = 'green'; 
+			} else if (memorizedNum >= 5) {
+				out = 'yellow';
+			}
+			return out;
+		}
+	});
