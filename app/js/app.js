b7772f1e6630e1108d3607f38ec29348cc71fc29
Johann Chen <johannchen@gmail.com>
2012-08-06 23:10:48 -0700

    about page

--- app/js/app.js
+++ app/js/app.js
@@ -5,27 +5,6 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/verses.html', controller: VersesCtrl});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
-
-// drag and drop file
-/*
-var import = document.getElementById('import');
-
-import.ondragover = function() { this.className = 'hover'; return false; };
-import.ondragend = function() { this.className = ''; return false; };
-import.ondrop = function(e) {
-  this.className = '';
-  e.preventDefault();
-
-  var file = e.dataTransfer.files[0],
-    reader = new FileReader();
-  reader.onload = function(event) {
-    console.log(event.target);
-  };
-
-  reader.readAsDataURL(file);
-
-  return false;
-};
-*/
