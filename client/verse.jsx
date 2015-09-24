00b3c6169b556d5815b18d65f9868626c1a2ba0e
Johann Chen <johannchen@gmail.com>
2015-09-24 16:18:55 -0700

    show dialog

--- client/verse.jsx
+++ client/verse.jsx
@@ -1,7 +1,18 @@
-var { Card, CardTitle, CardText, CardActions, FlatButton } = MUI;
+var { Card, CardTitle, CardText, CardActions, FlatButton, Dialog } = MUI;
+
 Verse = React.createClass({
+  getInitialState() {
+    return {
+      modal: false
+    }
+  },
   render() {
+    let standardActions = [
+      { text: 'Cancel' },
+      { text: 'Submit', onTouchTap: this._onDialogSubmit, ref: 'submit' }
+    ];
     return (
+      <div>
       <Card initiallyExpanded={true} className="verse">
         <CardTitle
           title={this.props.verse.title}
@@ -11,9 +22,22 @@ Verse = React.createClass({
         </CardText>
         <CardActions expandable={true}>
           <FlatButton label="Note"/>
-          <FlatButton label="Memorize"/>
+          <FlatButton label="Memorize" onTouchTap={this._handleDiaglogTouchTap}/>
         </CardActions>
       </Card>
+      <Dialog
+        ref="dialog"
+        title={this.props.verse.title}
+        actions={standardActions}
+        actionFocus="submit"
+        modal={this.state.modal}>
+        The actions in this window are created from the json that's passed in.
+      </Dialog>
+      </div>
     )
+  },
+
+  _handleDiaglogTouchTap() {
+    this.refs.dialog.show();
   }
 });
